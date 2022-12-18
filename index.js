dd9479e89c78d8a368a61f93b29a24cc0981c7cd
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2022-12-18 15:40:11 +0530

    Fix issues and test upload

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const core = require("@actions/core");
 const klawSync = require("klaw-sync");
+const fs = require("fs");
+const path = require("path");
+const { lookup } = require("mime-types");
 
 const ACCOUNT_ID = core.getInput("account_id", {
   required: true,
@@ -33,38 +36,34 @@ const S3 = new S3Client({
 
 const destinationDir = DESTINATION_DIR ?? "/";
 
-function upload(params) {
-  return new Promise((resolve) => {
-    S3.send(new PutObjectCommand({ Bucket: BUCKET_NAME, Key: params }));
-  });
+async function upload(params) {
+  await S3.send(new PutObjectCommand(params));
 }
-
-const paths = klawSync(newdir, {
-  nodir: true,
-});
 
 async function main() {
   const sourceDir = path.join(process.cwd(), SOURCE_DIR);
-
+  const paths = klawSync(SOURCE_DIR, {
+    nodir: true,
+  });
   return Promise.all(
     paths.map(async (p) => {
       const fileStream = fs.createReadStream(p.path);
-      const bucketPath = path.join(
-        destinationDir,
-        path.relative(sourceDir, p.path)
-      );
-      const signedUrl = await getSignedUrl(
+      const bucketPath = `${destinationDir}/${path.relative(
+        sourceDir,
+        p.path
+      )}`;
+
+      await getSignedUrl(
         S3,
         new PutObjectCommand({
-          Bucket: process.env.BUCKET_NAME,
+          Bucket: BUCKET_NAME,
           Key: bucketPath,
         }),
         { expiresIn: 3600 }
       );
 
       const params = {
-        Bucket: BUCKET,
-        ACL: "public-read",
+        Bucket: BUCKET_NAME,
         Body: fileStream,
         Key: bucketPath,
         ContentType: lookup(p.path) || "text/plain",
